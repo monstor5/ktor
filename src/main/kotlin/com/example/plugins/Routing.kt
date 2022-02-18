2b364d8aebc19994b494b4fdf89b77663463ca2d
monstor5 <zhoratazeev@gmail.com>
2022-02-18 10:07:52 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package com.example.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/cheanime") {
+            call.respondText("Hello World!")
+        }
+
+    }
+}
